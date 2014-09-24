d6c78566e23a0dc3df56975071c22ad2a05edf70
Ubuntu <ubuntu@ip-172-31-30-164.us-west-2.compute.internal>
2014-09-24 10:31:54 +0000

    get auth keys from file

--- sensor.py
+++ sensor.py
@@ -7,8 +7,15 @@ import twitter
 
 class Sensor(object):
 	def __init__(self,path="../auth.json"):
-		auth = json.loads(file(path).read())	
-		print auth
+		keys = json.loads(file(path).read())	
+		
+		auth = twitter.OAuth(keys['OAUTH_TOKEN'], keys['OAUTH_TOKEN_SECRET'],
+				keys['APP_KEY'], keys['APP_SECRET'])
+
+		self.api = twitter.Twitter(auth=auth)
+
+
+	
 
 
 s = Sensor()
