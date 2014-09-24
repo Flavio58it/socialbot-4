2b11cac866394baf59fec09ee80ba2ea9d9893b5
Ubuntu <ubuntu@ip-172-31-30-164.us-west-2.compute.internal>
2014-09-24 10:53:41 +0000

    Added bad_Friends function

--- sensor.py
+++ sensor.py
@@ -14,8 +14,17 @@ class Sensor(object):
 
 		self.api = twitter.Twitter(auth=auth)
 
+	def bad_friends(self):
+		# remove friends who do not follow back
+		friends = set(self.api.friends.ids()['ids'])
+		followers = set(self.api.followers.ids()['ids'])
+		bad_friends = list(friends - (friends.intersection(followers)))
+		f = open('../data/bad_friends.json','w')
+		f.write(json.dumps(bad_friends))
+		f.close()
 
 	
 
 
 s = Sensor()
+s.bad_friends()
