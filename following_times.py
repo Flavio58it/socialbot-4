5cac00376934136a97d11c5d308488a8fc83e455
Mads Eiler Hansen <mads@podio.com>
2014-11-19 14:43:34 +0100

    Added file to track when we had which amount of friends

---
+++ following_times.py
@@ -0,0 +1,31 @@
+import datetime as dt
+import pandas as pd
+
+current_friend_count = 542
+
+activity_fp = open('hiriactivity.log')
+act_dat = []
+format_day = '%Y-%d-%m'
+format_time = '%H-%M-%S'
+
+for line in activity_fp:
+	day, time, action, usr_id = line.strip().split(' ')
+	act_dat.append({'day':day, 'time':time, 'action':action, 'usr_id':usr_id})
+
+act_dat = pd.DataFrame(act_dat, columns = ['day', 'time', 'action', 'usr_id'])
+
+rev = range(len(act_dat))
+rev.reverse()
+
+no_fols = []
+
+for idx in rev:
+	if act_dat['action'][idx] == 'unfol':
+		current_friend_count += 1
+	elif act_dat['action'][idx] == 'fol':
+		current_friend_count -= 1
+	no_fols.append(current_friend_count)
+
+no_fols.reverse()
+act_dat['no_fols'] = no_fols
+
